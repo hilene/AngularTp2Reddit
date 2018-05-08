f0bc66e8eb80ee87211f6800fff9027fb0e01e9a
Hilene LIN <hlin@iorga.local>
2018-05-08 16:47:13 +0200

    ArticleService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,35 @@
 import { Component } from '@angular/core';
 import { Article } from './article/article.model';
+import { ArticleService } from './article.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+   providers: [ArticleService]
 })
 export class AppComponent {
   title = 'app';
-  articles: Article[];
+  article: Article[];
 
-  constructor(){
-    this.articles = [
+  constructor(service: ArticleService){
+    this.article = service.getArticles();
+     /*this.article = [
       new Article('Angular 2', 'http', 3),
       new Article('Angular 4', 'http', 4),
       new Article('Angular 5', 'http', 4)
-    ];
+    ];*/
   }
   addArticle(title: HTMLInputElement, link: HTMLInputElement){
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    this.articles.push(new Article(title.value, link.value, 0));
+    this.article.push(new Article(title.value, link.value, 0));
     title.value = '';
     link.value = '';
     return false;
   }
 
   sortedArticles(): Article[] {
-    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+    return this.article.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { Article } from './article/article.model';
 
 
 @Injectable()
 export class ArticleService {
 article: Article[];
-  constructor() { }
+  constructor() {
+    this.article = [
+      new Article('Angular 2', 'http', 3),
+      new Article('Angular 4', 'http', 4),
+      new Article('Angular 5', 'http', 4)
+    ];
+   }
 
 getArticles(){
   console.log(`ici le service !`);
   return this.article;
 }
 }
+
